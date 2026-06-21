bdc9f3aa0d338e3f5fe6a4efadd61a8dccb23cc6
Skydust <evann.jallet@gmail.com>
2026-06-21 21:58:29 +0200

    Adding the new DXVK HDR variable

--- src/tweaks/proton_wayland_hdr.rs
+++ src/tweaks/proton_wayland_hdr.rs
@@ -5,7 +5,8 @@ use crate::tweak_collector::PreparedCommand;
 
 #[tweak(name = "proton_wayland_hdr")]
 pub fn run(process: &mut Command, _: &mut PreparedCommand) {
-    process.env("PROTON_ENABLE_HDR", "1");
+    process.env("DXVK_HDR", "1");
+    process.env("PROTON_ENABLE_HDR", "1"); // deprecated
 
     if GPU::from_config().is_nvidia() {
         process.env("ENABLE_HDR_WSI", "1");
